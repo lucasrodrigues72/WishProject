48823a4351f690640379943626661eb96baa5e6e
mateo <watarycap@gmail.com>
2025-02-13 09:21:55 +0100

    Connexion BDD

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost"; // Adresse du serveur MySQL
+$dbname = "projet"; // Nom de la base de données
+$username = "root"; // Nom d'utilisateur MySQL (par défaut : root)
+$password = ""; // Mot de passe MySQL (par défaut vide sur localhost)
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
